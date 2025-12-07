705e62e3821956369bf97b14abef31ac3ea37fd2
Tutul Deb Roy <debroytutul@gmail.com>
2025-12-07 17:24:40 +0530

    Refactor gateway service configuration: remove unused security config, enhance logging setup, and clean up application properties

--- gateway-service/src/main/java/com/tu2l/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/tu2l/gateway/GatewayServiceApplication.java
@@ -1,13 +1,17 @@
 package com.tu2l.gateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
+	private static final Logger logger = LoggerFactory.getLogger(GatewayServiceApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("Starting Gateway Service Application...");
 		SpringApplication.run(GatewayServiceApplication.class, args);
+		logger.info("Gateway Service Application started successfully.");
 	}
-
 }
